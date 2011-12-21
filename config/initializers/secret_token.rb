1f1e959d6d8d7b3fb637305a93b4042eaac8809d
Dennis Lin <dennis.of.lin@gmail.com>
2011-12-20 22:53:07 -0800

    Added the Rails framework file structure. WHOO\!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomade::Application.config.secret_token = '887900d3a4f828ad0cd3cdcffde406945c636833f13931a2cc0830f4563fb93ce4c101710ccf4fc0aa548bd8aac49d76f246e58e8f8036af9d2adb68010431c7'
