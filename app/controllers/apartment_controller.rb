ab87ca6511470f3038819bccc5150952a40bbbcd
ken yu <kenniyu@gmail.com>
2012-03-25 14:24:22 -0700

    created apartment show page

--- app/controllers/apartment_controller.rb
+++ app/controllers/apartment_controller.rb
@@ -2,6 +2,19 @@ class ApartmentController < ApplicationController
   def show
     @apartment = Apartment.find_by_id(params[:id])
     redirect_to home_path if @apartment.nil?
+    @reviews = @apartment.reviews
+    ratings_hash = Hash.new(0)
+    
+    @reviews.each do |review|
+      ratings_hash["noise"] += review.noise
+      ratings_hash["condition"] += review.condition
+      # ratings_hash["security"] += review.security
+      ratings_hash["management"] += review.management
+    end
+    ratings_hash.each do |key, value|
+      ratings_hash[key] = value/@reviews.length
+    end
+    
   end
   
   
