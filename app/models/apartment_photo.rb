fad3afa4af1c828aea0cd389faa4776f90e0c20c
ken yu <kenniyu@gmail.com>
2012-04-02 19:32:44 -0700

    deploying to heroku

--- app/models/apartment_photo.rb
+++ app/models/apartment_photo.rb
@@ -6,7 +6,7 @@ class ApartmentPhoto < ActiveRecord::Base
     :url => ':s3_domain_url',
     :path => 'apartments/photos/:id/:style.:extension',
     :styles => {:thumb => "220x163#"},
-    :default_url => "/images/apartment.png"
+    :default_url => "/images/apartment.JPG"
     
   validates_attachment_content_type :photo, :content_type => ['image/gif', 'image/jpg', 'image/jpeg', 'image/png'], :if => Proc.new { |imports| !imports.photo_file_name.blank? }
   validates_attachment_size :photo, :less_than => 1.megabyte, :if => Proc.new { |imports| !imports.photo_file_name.blank? }
