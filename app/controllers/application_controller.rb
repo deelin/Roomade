7f8ab5d0185428c6daebf2e0dab5f643d93feb21
ken yu <kenniyu@gmail.com>
2012-04-27 14:42:50 -0700

    pushing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+  
   
   def redirect_if_logged_in
     # this gets called when a user tries to access any methods in the PublicController
     redirect_to home_path and return if current_user.present?
+  end
+  
+  def redirect_back_or(path = root_path)
+    redirect_to :back and return
+  rescue ActionController::RedirectBackError
+    redirect_to path and return
   end
       
   private
@@ -12,7 +20,10 @@ class ApplicationController < ActionController::Base
       root_path
     end
   
-    def after_sign_in_path_for(resource_or_scope)
-      home_path
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || root_path
     end
+
+    
+    
 end
