72820fd576b79dd3e192ac95f0217904f4a20a78
ken yu <kenniyu@gmail.com>
2012-04-29 22:40:59 -0700

    ordering admin backend

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,9 +2,9 @@ class AdminController < ApplicationController
   before_filter :check_admin
   
   def index
-    @apartments = Apartment.find(:all, :include => [:apartment_photos, :reviews])
-    @users = User.find(:all, :include => [:authentications, :reviews])
-    @reviews = Review.find(:all, :include => [:user, :apartment])
+    @apartments = Apartment.find(:all, :include => [:apartment_photos, :reviews], :order => "id asc")
+    @users = User.find(:all, :include => [:authentications, :reviews], :order => "id asc")
+    @reviews = Review.find(:all, :include => [:user, :apartment], :order => "id asc")
     
   end
 end
